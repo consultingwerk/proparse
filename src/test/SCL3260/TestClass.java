2941334fde958c77e2f0499cf22de8b4f7191c11
sebastianwandel <68946969+sebastianwandel@users.noreply.github.com>
2020-12-15 15:03:07 +0100

    Created TestClass
    Created a test-class for this issue

---
+++ src/test/SCL3260/TestClass.java
@@ -0,0 +1,40 @@
+package test.SCL3260;
+
+import java.io.File;
+
+import org.prorefactor.core.JPNode;
+import org.prorefactor.refactor.RefactorException;
+import org.prorefactor.treeparser.ParseUnit;
+
+import junit.framework.TestCase;
+
+public class TestClass extends TestCase {
+
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+
+	protected void tearDown() throws Exception {
+		super.tearDown();
+	}
+	
+	// For now just attempts to iterate through the tree and print the file
+	public void testMethod01() throws RefactorException {
+		java.io.File file = new File("C:\\Work\\Proparse\\Github\\proparse\\src\\test\\SCL3260\\testCode.p");
+		
+		ParseUnit pu = new ParseUnit(file);
+		pu.treeParser01();
+		JPNode node = pu.getTopNode();
+		
+		this.print(node);
+	}
+	
+	private void print(JPNode node) {
+		System.out.print(node.allLeadingHiddenText() + node.getText());
+		
+		if(node.firstChild() != null)
+			this.print(node.firstChild());
+		if (node.nextSibling() != null)
+			this.print(node.nextSibling());
+	}
+}
---
+++ src/test/SCL3260/testCode.p
@@ -0,0 +1,25 @@
+
+/*------------------------------------------------------------------------
+    File        : testCode.p
+    Purpose     :
+
+    Syntax      :
+
+    Description :
+
+    Author(s)   : Sebastian
+    Created     : Tue Dec 15 09:46:10 CET 2020
+    Notes       :
+  ----------------------------------------------------------------------*/
+
+/* ***************************  Definitions  ************************** */
+
+BLOCK-LEVEL ON ERROR UNDO, THROW.
+
+/* ********************  Preprocessor Definitions  ******************** */
+
+
+/* ***************************  Main Block  *************************** */
+
+VAR CHARACTER cText.
+
